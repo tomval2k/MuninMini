da136bf54288a8c824aa085f143876b2df4532a1
Tom <github@tomvalentine.net>
2014-04-05 15:32:42 +0100

    change gensub to gsub

--- plugins.d/irqstats.sh
+++ plugins.d/irqstats.sh
@@ -6,14 +6,27 @@ graph_args --base 1000 -l 0
 graph_vlabel interrupts
 graph_category system"
 
-	awk '/[0-9]+:/ { i=gensub(/:/, "", "g", $1); print "i" i ".label " $4 "\ni" i ".info Interrupt " i ", for device(s): " $4 "\ni" i ".min 0\ni" i ".type DERIVE"} \
-	/ERR:/ { print "iERR.label ERR\niERR.type DERIVE\niERR.min 0" } ' < /proc/interrupts
+	awk '/[0-9]+:/ {
+	#	i=gensub(/:/, "", "g", $1);
+	#	print "i" i ".label " $4 "\ni" i ".info Interrupt " i ", for device(s): " $4 "\ni" i ".min 0\ni" i ".type DERIVE"
+		gsub(/:/, "", $1);
+		print "i" $1 ".label " $4 "\ni" $1 ".info Interrupt " $1 ", for device(s): " $4 "\ni" $1 ".min 0\ni" $1 ".type DERIVE"
+	}
+	/ERR:/ {
+		print "iERR.label ERR\niERR.type DERIVE\niERR.min 0"
+	} ' < /proc/interrupts
 
 	if [ "$1" = "data" ]; then data; fi
 }
 
 data() {
-	awk '/[0-9]+:/ { print "i" gensub(/:/, "", "g", $1)  ".value " $2 } /ERR:/ { print "iERR.value " $2 }' < /proc/interrupts
+	awk '/[0-9]+:/ {
+		gsub(/:/, "", $1);
+		print "i" $1 ".value " $2
+	}
+	/ERR:/ {
+		print "iERR.value " $2
+	}' < /proc/interrupts
 }
 
 
